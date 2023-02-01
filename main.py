6e5009869ed4cfc75fbc3a717467fe86ecf3043b
malackapite <veress.marcell@protonmail.com>
2023-02-01 15:22:32 +0100

    21 eredmeny, ha nagyobb 21-nél

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 #megoldás
-def eredmeny():pass
+def eredmeny(jatekos, gep):
+    szoveg=""
+    if jatekos > 21 and gep > 21:
+        szoveg="döntetlen"
+    elif jatekos > 21:
+        szoveg="vesztettél"
+    elif gep > 21:
+        szoveg ="nyertél"
+    return szoveg
 #teszesetek
