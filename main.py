cf48a50533732b90f5e4551201a10453a027a6c1
malackapite <veress.marcell@protonmail.com>
2023-02-01 15:43:04 +0100

    21 eredmeny listakkal

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #megoldás
-def eredmeny(jatekos, gep):
+def eredmeny(jatekosLapok, gepLapok):
     szoveg=""
+    jatekos=pontszamitas(jatekosLapok)
+    gep=pontszamitas(gepLapok)
+
     if jatekos > 21 and gep > 21:
         szoveg="döntetlen"
     elif jatekos > 21:
@@ -8,4 +11,10 @@ def eredmeny(jatekos, gep):
     elif gep > 21:
         szoveg ="nyertél"
     return szoveg
+def pontszamitas(lista):
+    sum=0
+    for ix in lista:
+        sum+=ix
+    return sum
+    #return sum(lista)
 #teszesetek
